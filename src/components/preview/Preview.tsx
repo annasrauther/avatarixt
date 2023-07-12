695b98ac1003aba25f5fe50e20eecba5fa78b5dc
Annas Rauther <annasrauther95@gmail.com>
2023-07-13 00:30:54 +0530

    refactor: remove extra useEffects and intersection observer, rename as per Options context usage

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,11 +1,18 @@
+"use client"
 import { useContext, useEffect, useState } from 'react';
 import { BigHead } from '@bigheads/core';
-import { SelectedOptionsContext, SelectedOptionsContextType } from '@/context/selectedOptionsContext';
+import { OptionsContext } from '@/context/optionsContext';
 
 import styles from './Preview.module.css';
 
 const Preview = () => {
-  const { selectedOptions } = useContext<SelectedOptionsContextType>(SelectedOptionsContext);
+  const optionsContext = useContext(OptionsContext);
+
+  if (!optionsContext) {
+    throw new Error('OptionsContext is not provided.');
+  }
+
+  const { options } = optionsContext;
 
   const {
     accessory,
@@ -27,35 +34,11 @@ const Preview = () => {
     mask,
     mouth,
     skinTone,
-  } = selectedOptions;
+  } = options;
 
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
-    // Observer for checking intersection with viewport
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          setIsSticky(entry.isIntersecting);
-        });
-      },
-      { threshold: 0 }
-    );
-
-    const target = document.querySelector(`.${styles.previewContainer}`);
-    if (target) {
-      observer.observe(target);
-    }
-
-    return () => {
-      if (target) {
-        observer.unobserve(target);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    // Scroll event listener for updating sticky state
     const handleScroll = () => {
       setIsSticky(window.scrollY > 0);
     };
@@ -76,25 +59,25 @@ const Preview = () => {
         Download
       </button>
       <BigHead
-        accessory={accessory.value}
-        body={body.value}
-        clothing={clothing.value}
-        clothingColor={clothingColor.value}
-        eyebrows={eyebrows.value}
-        eyes={eyes.value}
-        faceMask={faceMask.value}
-        faceMaskColor={faceMaskColor.value}
-        facialHair={facialHair.value}
-        graphic={graphic.value}
-        hair={hair.value}
-        hairColor={hairColor.value}
-        hat={hat.value}
-        hatColor={hatColor.value}
-        lashes={lashes.value}
-        lipColor={lipColor.value}
-        mask={mask.value}
-        mouth={mouth.value}
-        skinTone={skinTone.value}
+        accessory={accessory?.value}
+        body={body?.value}
+        clothing={clothing?.value}
+        clothingColor={clothingColor?.value}
+        eyebrows={eyebrows?.value}
+        eyes={eyes?.value}
+        faceMask={faceMask?.value}
+        faceMaskColor={faceMaskColor?.value}
+        facialHair={facialHair?.value}
+        graphic={graphic?.value}
+        hair={hair?.value}
+        hairColor={hairColor?.value}
+        hat={hat?.value}
+        hatColor={hatColor?.value}
+        lashes={lashes?.value}
+        lipColor={lipColor?.value}
+        mask={mask?.value}
+        mouth={mouth?.value}
+        skinTone={skinTone?.value}
       />
     </div>
   );
