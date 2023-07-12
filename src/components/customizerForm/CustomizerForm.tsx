7bc4fa9c44a07ffa5ee65ce4bb8d813a91ad8c31
Annas Rauther <annasrauther95@gmail.com>
2023-07-13 00:29:17 +0530

    refactor: simplify rendering logic, rename as per Options context usage

--- src/components/customizerForm/CustomizerForm.tsx
+++ src/components/customizerForm/CustomizerForm.tsx
@@ -1,76 +1,71 @@
+"use client";
 import { useContext } from 'react';
 import styles from './CustomizerForm.module.css';
 import SwitchPill from '@/components/switch/SwitchPill';
 import ColorPicker from '@/components/colorPicker/ColorPicker';
-import Card from '@/components/card/Card';
-import { SelectedOptionsContext, SelectedOptionsContextType } from '@/context/selectedOptionsContext';
+import Card, { CardOption } from '@/components/card/Card';
 
-export interface ICustomizerFormProps {
+import { OptionsContext, OptionsContextType } from '@/context/optionsContext';
+const OptionsContextNonNull = OptionsContext as NonNullable<React.Context<OptionsContextType>>;
+
+export interface CustomizerFormProps {
   partMap: any;
 }
 
-const CustomizerForm = ({ partMap }: ICustomizerFormProps) => {
-  const { selectedOptions, handleOptionChange } = useContext<SelectedOptionsContextType>(SelectedOptionsContext);
+const CustomizerForm: React.FC<CustomizerFormProps> = ({ partMap }) => {
+  const { options, updateOption } = useContext<OptionsContextType>(OptionsContextNonNull);
 
-  const updateSwitchOption = (partKey: string, newValue: any) => {
-    // Only for Switch Component Events
-    handleOptionChange(partKey, newValue);
-  };
-
-  const renderComponent = (component: string, partKey: string, label: string, options: any[]) => {
-    switch (component) {
-      case 'switch':
-        return (
-          <div className={styles.formGroup} key={partKey}>
-            <h3 className={styles.label}>{label}</h3>
-            <div className={styles.switchContainer}>
-              <SwitchPill
-                value={selectedOptions[partKey].value}
-                onChange={(newValue: boolean) => updateSwitchOption(partKey, newValue)}
-              />
-            </div>
-          </div>
-        );
-      case 'color':
-        return (
-          <div className={styles.formGroup} key={partKey}>
-            <div className={styles.colorPickerContainer}>
-              <ColorPicker
-                partKey={partKey}
-                label={label}
-                value={selectedOptions[partKey].value}
-                colors={options}
-              />
-            </div>
-          </div>
-        );
-      case 'card':
-        return (
-          <div className={styles.formGroup} key={partKey}>
-            <h3 className={styles.label}>{label}</h3>
-            <div className={styles.cardContainer}>
-              <Card
-                partKey={partKey}
-                value={selectedOptions[partKey].value}
-                options={options}
-              />
-            </div>
-          </div>
-        );
-      default:
-        return null;
-    }
+  const updateSwitchOption = (partKey: string, newValue: boolean) => {
+    updateOption(partKey, newValue);
   };
 
   return (
     <div className={styles.container}>
       {Object.keys(partMap).map((partKey) => {
-        if (!selectedOptions[partKey].display) {
-          return true;
+        if (!options[partKey]?.display) {
+          return null;
         }
         const part = partMap[partKey];
-        const { label, component, options } = part;
-        return renderComponent(component, partKey, label, options)
+        const { label, component, options: partOptions } = part;
+        const componentOptions = options[partKey]?.value;
+        switch (component) {
+          case 'switch':
+            return (
+              <div className={styles.formGroup} key={partKey}>
+                <h3 className={styles.label}>{label}</h3>
+                <div className={styles.switchContainer}>
+                  <SwitchPill
+                    value={componentOptions}
+                    onChange={(newValue: boolean) => updateSwitchOption(partKey, newValue)}
+                  />
+                </div>
+              </div>
+            );
+          case 'color':
+            return (
+              <div className={styles.formGroup} key={partKey}>
+                <div className={styles.colorPickerContainer}>
+                  <ColorPicker
+                    partKey={partKey}
+                    label={label}
+                    value={componentOptions}
+                    colors={partOptions}
+                  />
+                </div>
+              </div>
+            );
+          case 'card':
+            return (
+              <div className={styles.formGroup} key={partKey}>
+                <h3 className={styles.label}>{label}</h3>
+                <div className={styles.cardContainer}>
+                  <Card partKey={partKey} options={partOptions as CardOption[]} />
+                </div>
+              </div>
+            );
+          default:
+            return null;
+        }
       })}
     </div>
   );
